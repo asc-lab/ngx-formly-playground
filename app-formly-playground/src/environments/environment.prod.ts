074757020f97a4e1a589f8f3361447523661e96c
SOFTWARE\achroscielewska <agnieszka.chroscielewska@altkom.pl>
2019-04-24 09:43:06 +0200

    angular app setup

---
+++ app-formly-playground/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import { convertToR3QueryMetadata } from '@angular/core/src/render3/jit/directive';
+
+export const environment = {
+  production: convertToR3QueryMetadata,
+  app_url: 'http://mn.lab.software.altkom.pl',
+  version: '0.1'
+};
